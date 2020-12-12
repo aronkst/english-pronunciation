ca65b74c6042a9740ae25c4604a22c47c23a81f6
Aron <aron.0504@hotmail.com>
2020-12-12 11:30:42 -0300

    create TrainingButton component

---
+++ src/components/TrainingButton.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Aux from "./Aux";
+import Button from "react-bootstrap/Button";
+
+const TrainingButton = (props) => {
+  return (
+    <Aux>
+      {props.active ? (
+        <Button
+          variant="primary"
+          className="float-right"
+          onClick={() => props.stopTraining()}
+        >
+          Stop training mode
+        </Button>
+      ) : (
+        <Button
+          variant="primary"
+          className="float-right"
+          onClick={() => props.beginTraining()}
+        >
+          Start training mode
+        </Button>
+      )}
+    </Aux>
+  );
+};
+
+export default TrainingButton;
